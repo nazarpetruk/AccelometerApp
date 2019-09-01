26b97f86d558b1b47966a88d4b0a8e176341dcef
nazar petruk <nazarpetruk15@gmail.com>
2019-09-01 16:01:05 +0200

    simple app showing data from accelerometer just to get in with CoreMotion

--- AccelometerApp/ViewController.swift
+++ AccelometerApp/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    var motionManager: CMMotionManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLbl)
+    }
+    func updateLbl(data: CMAccelerometerData?, error: Error?){
+        guard let accelerometerData = data else{return}
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        
+        let x = formatter.string(for: accelerometerData.acceleration.x)!
+        let y = formatter.string(for: accelerometerData.acceleration.y)!
+        let z = formatter.string(for: accelerometerData.acceleration.z)!
+        xLabel.text = "X: \(x)"
+        yLabel.text = "Y: \(y)"
+        zLabel.text = "Z: \(z)"
+        
     }
 
 
